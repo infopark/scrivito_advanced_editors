4ec58082fe671726267928dbc00f932d19357cb1
Gert Geidel <gert.geidel@infopark.de>
2015-07-06 14:53:10 +0200

    list editor also usable on attributes with type stringlists

--- app/assets/javascripts/scripts/scrivito_list_editor.js
+++ app/assets/javascripts/scripts/scrivito_list_editor.js
@@ -5,14 +5,15 @@
     App.ScrivitoListEditor = {
       // set selector for Editor
       selector: "[data-editor='scrivito-list-editor']",
-      
+
       // Function that will be called on scrivito load
       initFunction: function() { },
-      
+
       // set function triggert on click
       clickFunction: function(scrivito_tag, text, delimiter) {
         var delimiter = $(scrivito_tag).data('delimiter') || '|';
-        return scrivito_tag.scrivito('save', text.join(delimiter));
+        var content = scrivito_tag.data('scrivito-field-type') == 'string' ? text.join(delimiter) : text;
+        return scrivito_tag.scrivito('save', content);
       },
     };
 
@@ -22,8 +23,9 @@
         var elems = $(ScrivitoListEditor.selector);
         $.each(elems, function(index, elem) {
           var delimiter = $(elem).data('delimiter') || '|';
+          var content = $(elem).data('scrivito-field-type') == 'string' ? $(elem).html().split(delimiter) : $(elem).scrivito('content');
           $(elem).tagEditor({
-            initialTags: $(elem).html().split(delimiter),
+            initialTags: content,
             delimiter: delimiter,
             forceLowercase: false,
             maxLength: 1000,
