6c78da022e3b0df0a286de2b22359cfa082ad48d
Gert Geidel <gert.geidel@infopark.de>
2015-07-15 17:12:56 +0200

    Fix Textarea editor in diff mode

--- app/assets/javascripts/scripts/scrivito_textarea_editor.js
+++ app/assets/javascripts/scripts/scrivito_textarea_editor.js
@@ -7,14 +7,25 @@
       // set selector for Editor
       selector: '[data-editor=scrivito-textarea-editor]',
 
+      initFunction: function() {
+        var editors = $(textareaEditor.selector);
+        $.each(editors, function(i, editor) {
+          var textarea = $("<textarea></textarea>").val($(editor).html());
+          $(editor).html(textarea);
+        });
+      },
+
       blurFunction: function(cmsField) {
         var text = cmsField.val();
-        return cmsField.scrivito('save', text);
+        return cmsField.parent().scrivito('save', text);
       }
     };
 
     scrivito.on('load', function() {
-      return $('body').on('blur', textareaEditor.selector, function(event) {
+      if(scrivito.in_editable_view()) {
+        textareaEditor.initFunction();
+      }
+      return $('body').on('blur', textareaEditor.selector + " textarea", function(event) {
         if(scrivito.in_editable_view()) {
           textareaEditor.blurFunction($(event.target));
         }
