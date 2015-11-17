5717f0ee25ef4a15288de8d5f6dfd3c21fe513f4
Gert Geidel <gert.geidel@infopark.de>
2015-11-17 16:13:44 +0100

    Helper methods map to new code.

--- app/helpers/scrivito_advanced_editors/scrivito_advanced_tag_helper.rb
+++ app/helpers/scrivito_advanced_editors/scrivito_advanced_tag_helper.rb
@@ -2,23 +2,19 @@ module ScrivitoAdvancedEditors
   module ScrivitoAdvancedTagHelper
 
     def scrivito_multi_select_button_editor(obj, attribute, list=nil)
-      scrivito_button_editor(obj, attribute, 'select', list)
+      if list
+        scrivito_tag(:div, obj, attribute, data: {multi_select_list: list})
+      else
+        scrivito_tag(:div, obj, attribute)
+      end
     end
 
     def scrivito_toggle_button_editor(obj, attribute, list=nil)
-      scrivito_button_editor(obj, attribute, 'toggle', list)
-    end
-
-    def scrivito_button_editor(obj, attribute, type, list=nil)
-      list = enum_list(obj, attribute) if list.nil?
-
-      buttons = if block_given?
-        list.map { |elem| yield(elem) }.join('').html_safe
+      if list
+        scrivito_tag(:div, obj, attribute, data: {toggle_list: list})
       else
-        list.map { |elem| fallback_toggle_button(obj, attribute, elem, obj.send(attribute), type) }.join('').html_safe
+        scrivito_tag(:div, obj, attribute)
       end
-
-      content_tag :div, buttons, class: "button_list #{type}"
     end
 
     def scrivito_selectable_color_classes(class_name, attribute)
@@ -28,56 +24,13 @@ module ScrivitoAdvancedEditors
         fallback_colors
       end
 
-      colors.map { |color| color_hash(color) }
+      colors
     end
 
     private
 
     def fallback_colors
-      ['','transparent', 'black', 'gray', 'light-gray', 'red', 'green', 'blue', 'yellow']
-    end
-
-    def fallback_toggle_button(obj, attribute, elem, active, type)
-      content = elem.is_a?(Hash) ? elem : content_hash(elem)
-      scrivito_tag(:button, obj, attribute, class: (content[:css] + " " + css_class(elem, active)), style: content[:style], data: data_attribute(content, type)) do
-        content[:caption]
-      end
-    end
-
-    def content_hash(elem)
-      {
-        content: elem,
-        caption: (elem.present? ? elem.to_s : 'default'),
-        style: '',
-        css: ''
-      }
-    end
-
-    def color_hash(color)
-      {
-        content: color,
-        caption: (color.present? ? color : 'default'),
-        style: '',
-        css: "color-select #{color == "" ? "default" : color} #{ color.present? ? '' : 'transparent_bg' }",
-      }
-    end
-
-    def data_attribute(elem, type)
-      {
-        editor: (type == 'toggle' ? 'scrivito-toggle-button' : 'scrivito-multi-select-button'),
-        content: elem[:content],
-      }
-    end
-
-    def enum_list(obj, attribute)
-      obj.attribute_definitions[attribute].values
-    end
-
-    def css_class(elem, active)
-      return '' if active.nil?
-      return active == elem[:content] ? 'active' : '' if elem.is_a? Hash
-      return active == elem.to_s ? 'active' : '' if (active.is_a?(String) || active.is_a?(Fixnum))
-      active.include?(elem) ? 'active' : ''
+      ['', 'black', 'gray', 'light-gray', 'red', 'green', 'blue', 'yellow']
     end
   end
 end
