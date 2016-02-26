644fb3058cfbd01e3cab0f2b2d83ddc7ae3dcf71
Gert Geidel <gert.geidel@infopark.de>
2016-02-26 14:30:00 +0100

    Multiselect button uses js and css from multienum editor

--- app/assets/javascripts/scripts/multi_select_button.js
+++ app/assets/javascripts/scripts/multi_select_button.js
@@ -1,62 +1,61 @@
 (function($, App) {
   'use strict';
 
-  var ScrivitoMultiSelectButton = {
-    init_function: function(scrivito_tag) {
-      var content = $(scrivito_tag).scrivito('content');
-      var is_enum = $(scrivito_tag).is('[data-scrivito-field-type=multienum]');
-      var values = is_enum ? $(scrivito_tag).scrivito('allowed_values') : $(scrivito_tag).data('multi-select-list');
-      var captions = $(scrivito_tag).data('multi-select-caption');
+  var activate, handleClick, renderTemplate,
+  indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
 
-      $(scrivito_tag).addClass('button_list').addClass('select').html('');
-      return $.each(values, function(index, value) {
-        var css_class = ($.inArray(value, content) >= 0) ? 'active' : 'inactive'
-
-        if(!is_enum) css_class = (content.indexOf(value) > -1) ? 'active' : 'inactive'
-
-        var caption = (captions && captions[value]) ? captions[value] : value
-        $('<button></button>')
-          .addClass('scrivito-multi-select-button')
-          .addClass(css_class)
-          .data('content', value)
-          .html(caption)
-          .appendTo($(scrivito_tag));
-      });
+  scrivito.editors.multienum_editor = {
+    can_edit: function(element) {
+      var is_multienum = $(element).is('[data-scrivito-field-type=multienum]')
+      var has_list = $(element).is('[data-multi-select-list]');
+      return is_multienum || has_list;
     },
-
-    clickFunction: function(event) {
-      var newValue = $(event.currentTarget).data('content');
-      var scrivito_tag = $(event.currentTarget).parent();
-      var content = $(scrivito_tag).scrivito('content');
-      var array_content = Array.isArray(content) ? content : content.split('%|%')
-      if(content === "") array_content = []
-
-      if($.inArray(newValue, array_content) >= 0) {
-        array_content.splice( array_content.indexOf(newValue), 1 );
-      } else {
-        array_content.push(newValue);
-      }
-
-      var to_save = $(scrivito_tag).is('[data-scrivito-field-type=multienum]') ? array_content : array_content.join('%|%');
-
-      scrivito_tag.scrivito('save', to_save).then(function() {
-        $(event.currentTarget).toggleClass('active');
-      });
-    },
+    activate: function(element) {
+      return activate($(element));
+    }
   };
 
   scrivito.on('load', function() {
-    scrivito.define_editor("toggle_multi_select_editor", {
-      can_edit: function(element) {
-        var is_enum = $(element).is('[data-scrivito-field-type=multienum]')
-        var has_list = $(element).is('[data-multi-select-list]')
-        return is_enum || has_list;
-      },
-      activate: function(element) {
-        ScrivitoMultiSelectButton.init_function(element);
-        $(element).on('click', '.scrivito-multi-select-button', ScrivitoMultiSelectButton.clickFunction);
-      }
-    });
+    return scrivito.define_editor('multienum', scrivito.editors.enum_editor);
   });
+
+  activate = function(cmsField) {
+    var validValues = cmsField.data('multi-select-list') || cmsField.scrivito('valid_values');
+    var captions = cmsField.data('multi-select-caption');
+    cmsField.html(renderTemplate(cmsField.scrivito('content'), validValues, captions));
+    return cmsField.find('.scrivito_enum_editor li').on('click', function() {
+      return handleClick(cmsField, $(this));
+    });
+  };
+
+  renderTemplate = function(values, validValues, captions) {
+    var i, len, li, ul, validValue, caption;
+    ul = $('<ul class="scrivito_enum_editor scrivito_multi"></ul>');
+    for (i = 0, len = validValues.length; i < len; i++) {
+      validValue = validValues[i];
+      caption = (captions && captions[validValue]) ? captions[validValue] : validValue;
+      li = $('<li></li>');
+      li.text(caption);
+      li.data('scrivito-select-value', validValue);
+      if (indexOf.call(values, validValue) >= 0) {
+        li.addClass('scrivito_enum_active');
+      }
+      ul.append(li);
+    }
+    return ul;
+  };
+
+  handleClick = function(cmsField, clickedItem) {
+    var values;
+    clickedItem.toggleClass('scrivito_enum_active');
+    values = $.map(cmsField.find('li.scrivito_enum_active'), function(item) {
+      return $(item).data('scrivito-select-value');
+    });
+    cmsField.scrivito('save', values).done(function() {
+      cmsField.trigger('scrivito_editors:save');
+      return cmsField.trigger('scrivito_editors:blur');
+    });
+    return false;
+  };
 
 })(jQuery, this);
