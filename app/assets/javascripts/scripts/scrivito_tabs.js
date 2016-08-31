fc94838ebe90ba9d8706fb8a8a61ba4bd7d72278
Gert Geidel <gert.geidel@infopark.de>
2016-08-31 17:05:11 +0200

    Make first element acitve if not set, make it in mobile view visible if is set

--- app/assets/javascripts/scripts/scrivito_tabs.js
+++ app/assets/javascripts/scripts/scrivito_tabs.js
@@ -5,13 +5,19 @@
 
   scrivito.on('content', function(content) {
     var tab_list = $(content).find('.tab-list');
-    var active = tab_list.find('.active');
+    var active;
 
-    if(!as_accordion()) {
-      $.each(active, function(i,e) {
-        $($(e).data('panel-target')).addClass('active');
-      });
-    }
+    $.each(tab_list, function(i,e) {
+      active = $(e).find('.active');
+
+      if(active.length == 0) {
+        // first elem in tab_list if not in small view
+        active = as_accordion() ? [] : $($(e).find('li')[0]);
+      }
+
+      $(active).addClass('active');
+      $($(active).data('panel-target')).addClass('active');
+    });
 
     tab_list.on('click', 'li', function() {
       var target = $(this).data('panel-target');
