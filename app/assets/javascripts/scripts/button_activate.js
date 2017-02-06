af6f1674dae0bc75569e542fcab42c004a14dd3c
Gert Geidel <gert.geidel@infopark.de>
2017-02-06 11:02:06 +0100

    Fix tab toggle by toggle button

--- app/assets/javascripts/scripts/button_activate.js
+++ app/assets/javascripts/scripts/button_activate.js
@@ -12,9 +12,12 @@
 
     // initial activate listelems
     $.each($('.tab-list'), function(index, elem) {
-      id = '#'+$(elem).attr('id')
-      b = $('[data-tab-to-toggle="'+id+'"]');
-      activate(b);
+      id = '#'+$(elem).attr('id');
+      if(id !== undefined) {
+        b = $('[data-tab-to-toggle="'+id+'"]');
+
+        activate(b);
+      }
     });
   });
 
@@ -24,15 +27,19 @@
     var selector = $(list).data('tab-to-toggle');
     var tab = $(selector);
     var panel, allowedValues;
-
     $(tab).find('.active').removeClass('active');
 
     $.each(tab.find('li'), function(index, elem) {
       panel = $(elem).data('panel-target');
       $(panel).removeClass('active');
       allowedValues = $(elem).data('allowed-values');
+      // array in attribute as string to be an array anymore.
+      // seems to happen since rails update ... data as array is not an array in dom, " and ' are switch in dom => not correct json to parse
+      // [' \[\]] => remove all space, ', [ and ] with
+      allowedValues = typeof(allowedValues) === 'string' ? allowedValues.replace(/[' \[\]]/g, "").split(",") : allowedValues;
 
       if(allowedValues == undefined || $.inArray(value, allowedValues) > -1) {
+        debugger
         if($(button).hasClass('scrivito_enum_active')) {
           $(elem).removeClass('inactive');
         }
